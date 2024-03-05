09aceea7d8d8c249f6895e56a1a1a9c6ef2ba036
elvin2003 <elvinelvinelvin2003@gmail.com>
2024-03-05 21:02:31 +0400

    first commit

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require("socket.io")
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://127.0.0.1:5173",
+    methods: ["GET", "POST"],
+  }
+})
+
+io.on('connection', (socket) => {
+  //console.log('New client connected: ' + socket.id);
+  io.emit('user connected', `${socket.id} connected`);
+
+  // Event listener for when a client sends a message
+  socket.on('chat message', (message) => {
+    //console.log('Message received: ' + message);
+
+    // Broadcast the message to all connected clients
+    io.emit('chat message', message);
+  });
+
+  // Event listener for when a client disconnects
+  socket.on('disconnect', () => {
+    //console.log('Client disconnected: ' + socket.id);
+    io.emit('user disconnected', `${socket.id} disconnected`);
+  });
+});
+
+
+// app.get("/", (req, res) => {
+//   res.send("gett");
+// });
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ frontend/src/app.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'preact/hooks'
+import io from 'socket.io-client';
+const socket = io("http://localhost:5000");
+
+export function App() {
+  const [messages, setMessages] = useState([]);
+  const [messageInput, setMessageInput] = useState('');
+
+  useEffect(() => {
+    // Event listener for incoming messages
+    socket.on('chat message', (msg) => {
+      setMessages(prevMessages => [...prevMessages, msg]);
+    });
+
+    socket.on('user connected', (msg) => {
+      setMessages(prevMessages => [...prevMessages, msg]);
+    });
+
+    socket.on('user disconnected', (msg) => {
+      setMessages(prevMessages => [...prevMessages, msg]);
+    });
+
+
+    // Cleanup function to remove the event listener
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const sendMessage = () => {
+    if (messageInput.trim() !== '') {
+      socket.emit('chat message', `${socket.id}: ${messageInput}`); // Send the message to the server
+      setMessageInput(''); // Clear the input field
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        {messages.map((msg, index) => (
+          <div key={index}>{msg}</div>
+        ))}
+      </div>
+      <input
+        type="text"
+        value={messageInput}
+        onChange={(e) => setMessageInput(e.target.value)}
+      />
+      <button onClick={sendMessage}>Send</button>
+    </div>
+  )
+}
+
+export default App;
